342e2a2e2403d17c38bb2b9da111488196dc10da
SettingNo01 <leminhtrong0901@gmail.com>
2026-03-25 11:49:50 +0700

    Update page.tsx

--- src/app/dich-vu/[id]/page.tsx
+++ src/app/dich-vu/[id]/page.tsx
@@ -1,12 +1,10 @@
-export default async function Page({ params }) {
-  const { id } = await params;
-
+export default async function Page() {
   return (
     <div>
       <div className="text-center my-6">
         <h1 className="text-3xl font-bold">Giới thiệu</h1>
       </div>
-      <h1>ID: {id}</h1>
+      {/* <h1>ID: {id}</h1> */}
     </div>
   );
 }
