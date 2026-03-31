f4d0930e0034c131e2c54132aa69b5a852abc461
SettingNo01 <leminhtrong0901@gmail.com>
2026-03-31 09:13:41 +0700

    few css in header

--- src/components/ui/Header/header.tsx
+++ src/components/ui/Header/header.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/ui/Header/navbar";
 
 export default function Header() {
   return (
-    <header className="relative z-50 bg-white px-3 py-3 shadow-md sm:px-4">
+    <header className="relative z-50 bg-white px-3 py-3 border-b border-yellow-300 sm:px-4">
       <Navbar />
     </header>
   );
