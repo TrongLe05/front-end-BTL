8d4be5cd72df2988e0f194b8a112dd5a82a739c4
Trongle05 <leminhtrong0901@gmail.com>
2026-04-08 17:34:13 +0700

    Big update 3

--- src/app/(admin)/(dashboard)/admin/quan-ly-dich-vu/page.tsx
+++ src/app/(admin)/(dashboard)/admin/quan-ly-dich-vu/page.tsx
@@ -17,8 +17,7 @@ export default function Page() {
         <SiteHeaderAdmin title="Quản lý dịch vụ" />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-            </div>
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6"></div>
           </div>
         </div>
       </SidebarInset>
--- src/app/(admin)/(dashboard)/editor/xoa-bai-viet/page.tsx
+++ src/app/(admin)/(dashboard)/editor/xoa-bai-viet/page.tsx
@@ -4,7 +4,7 @@ import { ArticleDeleteTable } from "@/components/dashboard/editor/article-delete
 import { SidebarArticle } from "@/components/dashboard/editor/sidebar-article";
 import { SiteHeaderArticle } from "@/components/dashboard/editor/site-header-article";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-  import { getAdminArticles } from "@/lib/api/article";
+import { getAdminArticles } from "@/lib/api/article";
 import { Article } from "@/types";
 
 async function DeleteArticlePageContent() {
