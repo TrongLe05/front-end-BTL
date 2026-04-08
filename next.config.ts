6da8e287752559f0b589dff5b505cc606d2b1829
Trongle05 <leminhtrong0901@gmail.com>
2026-04-08 17:39:59 +0700

    fix không load được ảnh từ api

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // images: {
-  //   domains: ["api.tomhum07.me"],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.tomhum07.me",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
